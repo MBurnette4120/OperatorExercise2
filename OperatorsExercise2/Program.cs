091616a929c403626a1b6bcb635ce27287e2c6c7
MBurnette4120 <mikeburnette@rocketmail.com>
2020-06-22 14:47:53 -0400

    Add project files.

---
+++ OperatorsExercise2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OperatorsExercise2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //var firstNum = 17;
+            //var secondNum = 4;
+            //var quotient = firstNum / secondNum;
+            //var remainder = firstNum % secondNum;
+
+            //Console.WriteLine($"{firstNum} / {secondNum} equals {quotient} remainder {remainder}");
+            //Console.WriteLine($"The remainder is {remainder}");
+
+            Console.WriteLine($"Please enter a radius to calculate area of a circle:");
+
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+            var area = AreaOfCircle(radius);
+
+            Console.WriteLine($"{area}");
+
+
+        }
+        public static double AreaOfCircle(double radius)
+        {
+
+            return (Math.PI) * (radius* radius);
+        }
+
+
+
+
+}
+}
